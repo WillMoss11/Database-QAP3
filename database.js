4b9c3fe9e1ba2fcccbde5323dff3fa14a57c3353
WillMoss11 <162141859+WillMoss11@users.noreply.github.com>
2024-12-03 19:49:23 -0330

    Changed db password from my own, forgot

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "booksDB", // Your PostgreSQL database
-  password: "LucWill53",
+  password: "1234",
   port: 5432,
 });
 
